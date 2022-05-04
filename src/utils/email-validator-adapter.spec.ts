afa4adec1ab770b6a0ea17014711112750b8c4b2
Gustavo Miguel <gu.rmiguel@gmail.com>
2022-05-04 00:14:29 -0300

    test: ensure EmailValidatorAdapter returns false if valdiator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,23 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+
+describe('EmailValidator Adapter', () => {
+  interface SutTypes {
+    sut: EmailValidatorAdapter
+  }
+
+  const makeSut = (): SutTypes => {
+    const sut = new EmailValidatorAdapter()
+
+    return {
+      sut,
+    }
+  }
+
+  test('Should return false if validator returns false', () => {
+    const { sut } = makeSut()
+
+    const isValid = sut.isValid('invalid_email@mail.com')
+
+    expect(isValid).toBe(false)
+  })
+})
