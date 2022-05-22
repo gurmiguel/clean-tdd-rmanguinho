e312f2fdc0d6d017372183623c591190fa6a30dc
Gustavo Miguel <gu.rmiguel@gmail.com>
2022-05-22 19:23:17 -0300

    test: ensure LogControllerDecorator calls controller handle

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,42 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return {
+        statusCode: 200,
+        body: {
+          ok: true,
+        },
+      }
+    }
+  }
+
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return { sut, controllerStub }
+}
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest: HttpRequest = {
+      body: {
+        any: 'thing',
+      },
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
